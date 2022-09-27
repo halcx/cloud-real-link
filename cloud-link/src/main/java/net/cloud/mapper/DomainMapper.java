a71610a583fb0ca74a5e219ff01d16d946f3507a
LyWangxiaohan <uestc_wangxiaohan@163.com>
2022-09-27 14:06:29 +0800

    feat(*): 代码生成

---
+++ cloud-link/src/main/java/net/cloud/mapper/DomainMapper.java
@@ -0,0 +1,16 @@
+package net.cloud.mapper;
+
+import net.cloud.model.DomainDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Wxh
+ * @since 2022-09-27
+ */
+public interface DomainMapper extends BaseMapper<DomainDO> {
+
+}
---
+++ cloud-link/src/main/java/net/cloud/mapper/GroupCodeMappingMapper.java
@@ -0,0 +1,17 @@
+package net.cloud.mapper;
+
+import net.cloud.model.GroupCodeMappingDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Wxh
+ * @since 2022-09-27
+ */
+public interface GroupCodeMappingMapper extends BaseMapper<GroupCodeMappingDO> {
+
+}
