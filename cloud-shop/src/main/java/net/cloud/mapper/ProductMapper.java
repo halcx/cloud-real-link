295a65c8e69f72db1335e22e975b55b801905170
LyWangxiaohan <uestc_wangxiaohan@163.com>
2022-10-15 13:29:10 +0800

    feat(*): bug fix

---
+++ cloud-shop/src/main/java/net/cloud/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package net.cloud.mapper;
+
+import net.cloud.model.ProductDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Wxh
+ * @since 2022-10-15
+ */
+public interface ProductMapper extends BaseMapper<ProductDO> {
+
+}
---
+++ cloud-shop/src/main/java/net/cloud/model/ProductDO.java
@@ -0,0 +1,85 @@
+package net.cloud.model;
+
+import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Wxh
+ * @since 2022-10-15
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("product")
+public class ProductDO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+      @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 商品标题
+     */
+    private String title;
+
+    /**
+     * 详情
+     */
+    private String detail;
+
+    /**
+     * 图片
+     */
+    private String img;
+
+    /**
+     * 产品层级：FIRST青铜、SECOND黄金、THIRD钻石
+     */
+    private String level;
+
+    /**
+     * 原价
+     */
+    private BigDecimal oldAmount;
+
+    /**
+     * 现价
+     */
+    private BigDecimal amount;
+
+    /**
+     * 工具类型 short_link、qrcode
+     */
+    private String pluginType;
+
+    /**
+     * 日次数：短链类型
+     */
+    private Integer dayTimes;
+
+    /**
+     * 总次数：活码才有
+     */
+    private Integer totalTimes;
+
+    /**
+     * 有效天数
+     */
+    private Integer validDay;
+
+    private Date gmtModified;
+
+    private Date gmtCreate;
+
+
+}
