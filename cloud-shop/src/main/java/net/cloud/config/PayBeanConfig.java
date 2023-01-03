901485cb6e3184079e57fda5cd2f7cea6c4f8b17
LyWangxiaohan <uestc_wangxiaohan@163.com>
2023-01-03 13:50:49 +0800

    feat(*): 微信证书和httpclient构建

--- cloud-shop/src/main/java/net/cloud/config/PayBeanConfig.java
+++ cloud-shop/src/main/java/net/cloud/config/PayBeanConfig.java
@@ -1,7 +1,14 @@
 package net.cloud.config;
 
+import com.wechat.pay.contrib.apache.httpclient.WechatPayHttpClientBuilder;
+import com.wechat.pay.contrib.apache.httpclient.auth.PrivateKeySigner;
+import com.wechat.pay.contrib.apache.httpclient.auth.ScheduledUpdateCertificatesVerifier;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Credentials;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Validator;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
@@ -9,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
@@ -46,4 +54,40 @@ public class PayBeanConfig {
             throw new RuntimeException("⽆效的密钥格式");
         }
     }
+
+    /**
+     * 定时获取微信签名验证器，自动获取微信平台证书（证书里面包括微信平台公钥）
+     * @return
+     */
+    @Bean
+    public ScheduledUpdateCertificatesVerifier getCertificatesVerifier(){
+        // 使用定时更新的签名验证器，不需要传入证书
+        ScheduledUpdateCertificatesVerifier verifier = null;
+        try {
+            verifier = new ScheduledUpdateCertificatesVerifier(
+                    new WechatPay2Credentials(payConfig.getMchId(), new PrivateKeySigner(payConfig.getMchSerialNo(), getPrivateKey())),
+                    payConfig.getApiV3Key().getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            log.error("定时获取微信签名验证器失败:{}",e);
+        }
+        return verifier;
+    }
+
+    /**
+     * 获取http请求对象
+     * @return
+     */
+    @Bean("getWechatPayClient")
+    public CloseableHttpClient getWechatPayClient(ScheduledUpdateCertificatesVerifier verifier) throws IOException {
+        WechatPayHttpClientBuilder builder = WechatPayHttpClientBuilder.create()
+                .withMerchant(payConfig.getMchId(), payConfig.getMchSerialNo(), getPrivateKey())
+                .withValidator(new WechatPay2Validator(verifier));
+        // ... 接下来，你仍然可以通过builder设置各种参数，来配置你的HttpClient
+
+        // 通过WechatPayHttpClientBuilder构造的HttpClient，会自动的处理签名和验签，并进行证书自动更新
+        CloseableHttpClient httpClient = builder.build();
+
+        return httpClient;
+    }
+
 }
